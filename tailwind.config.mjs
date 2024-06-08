07cba96e23db11524bb766fe19e616239231f298
AlexSh1mada <alexbuerom@gmail.com>
2024-06-08 04:55:59 -0600

    Added styling, fixed responsive 🥸

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,18 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			"animation": {
+				"text-gradient": "text-gradient 1.5s linear infinite"
+			  },
+			  "keyframes": {
+				"text-gradient": {
+				  "to": {
+					"backgroundPosition": "200% center"
+				  }
+				}
+			  }
+		},
 	},
 	plugins: [],
 }
