c4bde9b57b56bacdb0cbd502b36f76159b087108
AlexSh1mada <alexbuerom@gmail.com>
2024-06-10 04:12:45 -0600

    Added 2 more projects, modified stylings 🛠️🥸

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	darkMode: "class",
 	theme: {
 		extend: {
-			"animation": {
-				"text-gradient": "text-gradient 1.5s linear infinite"
-			  },
-			  "keyframes": {
+			animation: {
+				"text-gradient": "text-gradient 1.5s linear infinite",
+				"background-shine": "background-shine 2s linear infinite",
+			},
+			keyframes: {
 				"text-gradient": {
-				  "to": {
-					"backgroundPosition": "200% center"
-				  }
-				}
-			  }
+					to: {
+						backgroundPosition: "200% center",
+					},
+				},
+				"background-shine": {
+					from: {
+						backgroundPosition: "0 0",
+					},
+					to: {
+						backgroundPosition: "-200% 0",
+					},
+				},
+			},
 		},
 	},
 	plugins: [],
-}
+};
